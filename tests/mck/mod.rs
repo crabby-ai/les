8c9302a7e0a4ffa2abb5bec4d4b349fb3562342b
vasanthkumar <itsparser@gmail.com>
2022-12-29 01:20:19 +0530

    added inital test case

--- tests/mck/mod.rs
+++ tests/mck/mod.rs
@@ -9,7 +9,7 @@ use les::{
 };
 use rust_decimal::prelude::*;
 use les::exchange::binance::{Binance, BinanceCredentials, BinanceParameters};
-use les::exchange::binance::model::TradeHistoryReq;
+use les::exchange::binance::model::{KlineParams, KlineSummaries, TradeHistoryReq};
 
 async fn get_current_price(exchange: &impl Exchange, market_pair: &MarketPair, multiplier: f32) -> Decimal {
     let market_pair = market_pair.clone();
@@ -25,11 +25,11 @@ async fn get_current_price(exchange: &impl Exchange, market_pair: &MarketPair, m
 async fn init() -> Binance {
     dotenv().ok();
     Binance::new(BinanceParameters {
-        environment: Default::default(),
         credentials: Some(BinanceCredentials {
             api_key: env::var("BINANCE_API_KEY").expect("Couldn't get environment variable"),
             api_secret: env::var("BINANCE_API_SECRET").expect("Couldn't get environment variable"),
         }),
+        ..Default::default()
     })
     .await
     .expect("Failed to create Client")
@@ -38,16 +38,29 @@ async fn init() -> Binance {
 #[tokio::test]
 async fn trade_history() {
     let exchange = init().await;
+    let page = Paginator {
+        start_time: Some(),
+        end_time: None,
+        limit: Some(100),
+        after: None,
+        ..Default::default()
+    };
     let params = TradeHistoryReq {
         paginator: None,
         symbol: String::from("BNBBTC"),
     };
-
-    let resp = exchange
+    let b = exchange
         .inner_client()
-        .expect("Couldn't get inner time.")
-        .trade_history(&params)
-        .await
-        .expect("Couldn't trade history.");
+        .expect("Couldn't get inner time.");
+
+    let p = KlineParams{
+        symbol: "BNBBTC".to_string(),
+        interval: "1m".to_string(),
+        paginator: None,
+    };
+    let resp = b.get_klines(&p).await.expect("Couldn't trade history.");
+    println!("{:?}", resp);
+
+    let resp = b.trade_history(&params).await.expect("Couldn't trade history.");
     println!("{:?}", resp);
 }
